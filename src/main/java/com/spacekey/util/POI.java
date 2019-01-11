fa7418d758f5856df94fab7afb9e67c3502c5811
Wang Jikun <wagjk@hku.hk>
2019-01-11 16:57:37 +0800

    update POI model

--- src/main/java/com/spacekey/util/POI.java
+++ src/main/java/com/spacekey/util/POI.java
@@ -1,5 +1,11 @@
 package com.spacekey.util;
 
 public class POI {
-
+	public String name;
+	public int numOfReviews;
+	public String keyword;
+	public String keyword2;
+	public String address;
+	public double lat;
+	public double lng;
 }
